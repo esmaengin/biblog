c7560423a6466882366fee20ae883cdf2f1ffc25
Esma Engin <esmaengin91@gmail.com>
2023-01-03 15:20:09 +0300

    edited wrong function name

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import cors from "cors";
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.url({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.get("/", (req, res)=>{
